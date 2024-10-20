cbb2f3c1548fbb0a4d142da7a7301e81b632be96
huhehuhehu <70438266+huhehuhehu@users.noreply.github.com>
2024-10-20 17:04:33 +0700

    upload_product_image removed

--- testok/urls.py
+++ testok/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('insert_product/', insert_product),
     path('delete_all/', DeleteAll.as_view()),
     path('products/<str:brand_name>/<uuid:product_id>/', ProductDetailView.as_view()),
-    path('upload_img/', upload_product_image),
+    # path('upload_img/', upload_product_image),
     path('get_user_details/', GetUserDetails.as_view()),
     path('all_orders/', OrderSummaryView.as_view()),
     path('get_order/<uuid:order_id>/', OrderSummaryView.as_view()),
